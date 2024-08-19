607e1a6e1340878f883656d61f7df85a9e8c05a9
Kazllin <rm87320@estudante.fieb.edu.br>
2024-08-19 02:11:29 -0300

    slide

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,5 +1,17 @@
 import './inicio.css'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
 export default function Inicio(){
+
+  const setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll:1
+  };
+
   return(
     <div className="page">
       <div className="container_page">
@@ -8,6 +20,22 @@ export default function Inicio(){
           <h1 className='titulo_inicio'>Tudo o que você<br/> precisa para a escola</h1>
           <p className='subtitulo_inicio'>Explore our curated selection of high-quality school supplies for<br/> all your educational needs.</p>
             <button className='btn_inicio'>COMPRE AGORA</button>
+        </div>
+
+        <div className="slider_inicio">
+          <Slider {...setting}>
+            <div className="carousel_inicio">
+              <img src="" alt="" />
+            </div>
+
+            <div className="carousel_inicio">
+              <img src="" alt="" className='img_inicio' />
+            </div>
+
+            <div className="carousel_inicio">
+              <img src="" alt="" />
+            </div>
+          </Slider>
         </div>
           </div>
 
