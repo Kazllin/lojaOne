97137a3cf2dd2d92cd55eef62c749da388647e90
Kazllin <rm87320@estudante.fieb.edu.br>
2024-08-19 10:24:24 -0300

    aplicativo

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,12 +1,17 @@
 import './inicio.css'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
+import d from "../../components/img/icons/logar.png"
+import Slider from "react-slick"
 
 export default function Inicio(){
 
   const setting = {
     dots: true,
     infinite: true,
+    arrows:false,
+    autoplay: true,
+    autoplaySpeed:300,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll:1
@@ -25,15 +30,15 @@ export default function Inicio(){
         <div className="slider_inicio">
           <Slider {...setting}>
             <div className="carousel_inicio">
-              <img src="" alt="" />
+              <img src={d} alt="" className='img_inicio' />
             </div>
 
             <div className="carousel_inicio">
-              <img src="" alt="" className='img_inicio' />
+              <img src={d} alt="" className='img_inicio' />
             </div>
 
             <div className="carousel_inicio">
-              <img src="" alt="" />
+              <img src={d} alt="" className='img_inicio' />
             </div>
           </Slider>
         </div>
