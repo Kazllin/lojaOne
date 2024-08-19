502b8d8d3ae50629fe3069b20981ce99f507bdfb
Kazllin <141282238+Kazllin@users.noreply.github.com>
2024-08-19 04:43:22 +0000

    funcional

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,7 +1,21 @@
+import './inicio.css'
 export default function Inicio(){
   return(
     <div className="page">
-      
+      <div className="container_page">
+        <div className='container3'>
+          <div className='frases_inicio'>
+          <h1 className='titulo_inicio'>Tudo o que você<br/> precisa para a escola</h1>
+          <p className='subtitulo_inicio'>Explore our curated selection of high-quality school supplies for<br/> all your educational needs.</p>
+            <button className='btn_inicio'>COMPRE AGORA</button>
+        </div>
+          </div>
+
+        
+        <div className='container4'>
+          
+          </div>
+      </div>
     </div>
   )
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/header/index";
-import Footer from "./components/footer/index";
+
 import Inicio from "./pages/inicio/index";
 import Cadastro from "./pages/cadastro/index";
 import Produto from "./pages/cadastroProduto/index";
+import Layout from "./components/layout";
 
 export default function Routessistem() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Layout/>
         <Routes>
         <Route path='/' element={<Inicio/>}/>
+          <Route path='*' element={<Inicio/>}/>
           <Route path='/produto' element={<Produto/>}/>
           <Route path='/cadastro' element={<Cadastro/>}/>
         </Routes>
-      <Footer/>
+      <Layout/>
     </BrowserRouter>
   );
 }
