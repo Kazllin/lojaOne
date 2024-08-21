ec2830af72f8cd30601dc1f6698968dfe55f0810
Kazllin <rm87320@estudante.fieb.edu.br>
2024-08-21 11:50:51 -0300

    resposivo lixo

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -4,50 +4,97 @@ import "slick-carousel/slick/slick-theme.css"
 import d from "../../components/img/icons/logar.png"
 import Slider from "react-slick"
 
-export default function Inicio(){
+export default function Inicio() {
 
   const setting = {
     dots: true,
     infinite: true,
-    arrows:false,
+    arrows: false,
     autoplay: true,
-    autoplaySpeed:300,
+    autoplaySpeed: 300,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll:1
+    slidesToScroll: 1
   };
 
-  return(
+  return (
     <div className="page">
       <div className="container_page">
         <div className='container3'>
           <div className='frases_inicio'>
-          <h1 className='titulo_inicio'>Tudo o que você<br/> precisa para a escola</h1>
-          <p className='subtitulo_inicio'>Explore our curated selection of high-quality school supplies for<br/> all your educational needs.</p>
+            <h1 className='titulo_inicio'>Tudo o que você<br /> precisa para a escola</h1>
+            <p className='subtitulo_inicio'>Explore our curated selection of high-quality school supplies for<br /> all your educational needs.</p>
             <button className='btn_inicio'>COMPRE AGORA</button>
-        </div>
-
-        <div className="slider_inicio">
-          <Slider {...setting}>
-            <div className="carousel_inicio">
-              <img src={d} alt="" className='img_inicio' />
-            </div>
-
-            <div className="carousel_inicio">
-              <img src={d} alt="" className='img_inicio' />
-            </div>
-
-            <div className="carousel_inicio">
-              <img src={d} alt="" className='img_inicio' />
-            </div>
-          </Slider>
-        </div>
           </div>
 
-        
-        <div className='container4'>
-          
+          <div className="slider_inicio">
+            <Slider {...setting}>
+              <div className="carousel_inicio">
+                <img src={d} alt="" className='img_inicio' />
+              </div>
+
+              <div className="carousel_inicio">
+                <img src={d} alt="" className='img_inicio' />
+              </div>
+
+              <div className="carousel_inicio">
+                <img src={d} alt="" className='img_inicio' />
+              </div>
+            </Slider>
           </div>
+        </div>
+
+
+        <div className="container4">
+          <h1 className="carde_titulo">Popular Products</h1>
+          <div className="carde_grid">
+            <div className="carde_inicio">
+              <img src="" alt="" className="carde_img" />
+              <div className="carde_info">
+                <h2 className="carde_nome">Roupas</h2>
+                <p className="carde_descricao">Camisa</p>
+                <p className="carde_preco">12</p>
+                <button className="carde_add"></button>
+              </div>
+            </div>
+          </div>
+
+          <div className="carde_grid">
+            <div className="carde_inicio">
+              <img src="" alt="" className="carde_img" />
+              <div className="carde_info">
+                <h2 className="carde_nome">Roupas</h2>
+                <p className="carde_descricao">Camisa</p>
+                <p className="carde_preco">12</p>
+                <button className="carde_add"></button>
+              </div>
+            </div>
+          </div>
+
+          <div className="carde_grid">
+            <div className="carde_inicio">
+              <img src="" alt="" className="carde_img" />
+              <div className="carde_info">
+                <h2 className="carde_nome">Roupas</h2>
+                <p className="carde_descricao">Camisa</p>
+                <p className="carde_preco">12</p>
+                <button className="carde_add"></button>
+              </div>
+            </div>
+          </div>
+
+          <div className="carde_grid">
+            <div className="carde_inicio">
+              <img src="" alt="" className="carde_img" />
+              <div className="carde_info">
+                <h2 className="carde_nome">Roupas</h2>
+                <p className="carde_descricao">Camisa</p>
+                <p className="carde_preco">12</p>
+                <button className="carde_add"></button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
